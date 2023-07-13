a22e63a88721555be2dc6575685b13207ec9f1c7
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-13 09:20:18 +0500

    func-5

--- bin/basic_function05.dart
+++ bin/basic_function05.dart
@@ -1,1 +1,11 @@
 // Create a function called `getFullName` that returns a `String`. The function should return your full name.
+
+
+String getFullName(){
+    return 'Nodir';
+}
+
+void main(){
+    print(getFullName());
+}
+
